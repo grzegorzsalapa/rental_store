e93e940e8ed556bcfe7506b185925cc4c6f2e3d6
Greg <gregory.bobrovsky@gmail.com>
2023-02-22 19:01:01 +0100

    Added one of three calculators.

--- rental_store/calculators.py
+++ rental_store/calculators.py
@@ -1,11 +1,35 @@
 from rental_store.inventory import Film
 from rental_store.client import Client
+from datetime import date
 
 
 class PriceCalculator:
 
+    premium_price = 40
+    basic_price = 30
+
     def calculate_rent_charge(self, film: Film, up_front_days):
-        return 40, "SEK"
+
+        if film.film_type == "New release":
+            return PriceCalculator.premium_price * up_front_days, "SEK"
+
+        elif film.film_type == "Regular films":
+            return 30, "SEK"
+
+        elif film.film_type == "Old films":
+            return 20, "SEK"
 
     def calculate_rent_surcharge(self, film: Film, client: Client):
-        return 30, "SEK"
+
+        if film.film_type == "New release":
+            for item in client.rent_ledger:
+                if item["id"] == film.film_id and "date_of_return" not in item.keys():
+                    rent_duration = (date.today() - item["date_of_rent"]).days
+
+            return PriceCalculator.premium_price * rent_duration, "SEK"
+
+        elif film.film_type == "Regular films":
+            return 31, "SEK"
+
+        elif film.film_type == "Old films":
+            return 21, "SEK"
