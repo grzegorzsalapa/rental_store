ffcb307740c1a4def886d707988a025ab028736a
Greg <gregory.bobrovsky@gmail.com>
2023-02-27 14:02:33 +0100

    All calculators added.

--- rental_store/calculator.py
+++ rental_store/calculator.py
@@ -1,19 +1,28 @@
 from rental_store.data_models import Film, PriceList
 from datetime import date
 
+regular_flat_days = 3
+old_flat_days = 5
 
 def calculate_rent_charge(price_list: PriceList, film: Film, up_front_days: int):
 
     if film.type == "New release":
+
         charge = price_list.premium_price * up_front_days
 
         return charge, price_list.currency
 
     elif film.type == "Regular":
-        return 30, "SEK"
+
+        charge = max(regular_flat_days, up_front_days) * price_list.basic_price
+
+        return charge, price_list.currency
 
     elif film.type == "Old":
-        return 20, "SEK"
+
+        charge = max(old_flat_days, up_front_days) * price_list.basic_price
+
+        return charge, price_list.currency
 
 
 def calculate_rent_surcharge(price_list: PriceList, film: Film, up_front_days: int, date_of_rent: date):
@@ -28,7 +37,19 @@ def calculate_rent_surcharge(price_list: PriceList, film: Film, up_front_days: i
         return charge, price_list.currency
 
     elif film.type == "Regular":
-        return 31, "SEK"
+
+        rent_duration = (date.today() - date_of_rent).days
+        overdue = max(0, rent_duration - max(regular_flat_days, up_front_days))
+
+        charge = overdue * price_list.basic_price
+
+        return charge, price_list.currency
 
     elif film.type == "Old":
-        return 21, "SEK"
+
+        rent_duration = (date.today() - date_of_rent).days
+        overdue = max(0, rent_duration - max(old_flat_days, up_front_days))
+
+        charge = overdue * price_list.basic_price
+
+        return charge, price_list.currency
