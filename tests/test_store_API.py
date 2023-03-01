2cb1e8655b6aaae6885dcd8b79387efd07db5855
Greg <gregory.bobrovsky@gmail.com>
2023-03-01 11:27:31 +0100

    Test for get_customers.

--- tests/test_store_API.py
+++ tests/test_store_API.py
@@ -3,7 +3,7 @@ import pytest
 from fastapi.testclient import TestClient
 from rental_store.store_api import store
 from unittest.mock import patch
-from rental_store.data_models import Inventory, Film, Ledger, Customer, PriceList, RentalRecord, \
+from rental_store.data_models import Inventory, Film, Customer, PriceList, RentalRecord, \
     FilmRentResponseModel,\
     FilmRentResponseItemModel,\
     FilmReturnResponseModel,\
@@ -351,6 +351,68 @@ def test_end2end_get_customer():
                  'date_of_return': None,
                  'surcharge': None
                  }
+            ]
+        }
+
+    arrangement()
+    action_result = action()
+    assertion(action_result)
+
+
+def test_end2end_get_customers():
+
+    def arrangement():
+
+        item_0 = Film(id=0, title="Matrix 11", type="New release", items_total=50)
+        item_1 = Film(id=1, title="Spider Man", type="Regular", items_total=20)
+        rental_store.repositories.data_storage.inventory = Inventory(films=[item_0, item_1])
+        rental_store.repositories.data_storage.customers = [Customer(id=0), Customer(id=9), Customer(id=16), Customer(id=17)]
+        rental_store.repositories.data_storage.ledger.rentals = [
+            RentalRecord(
+                request_id='bab7010e-7803-468f-807b-6f4252a57178',
+                customer_id=9,
+                film_id=1,
+                date_of_rent='2023-01-11',
+                up_front_days=3,
+                charge=120
+            )
+        ]
+
+    def action():
+        response = test_client.get("/customers")
+
+        return response
+
+    def assertion(response):
+        assert response.json() == {
+            'customers': [
+                {
+                    'id': 0,
+                    'rentals': []
+                },
+                {
+                    'id': 9,
+                    'rentals': [
+                        {
+                            'charge': 120,
+                            'customer_id': 9,
+                            'date_of_rent': '2023-01-11',
+                            'date_of_return': None,
+                            'film_id': 1,
+                            'request_id': 'bab7010e-7803-468f-807b-6f4252a57178',
+                            'surcharge': None,
+                            'up_front_days': 3
+                        }
+                    ]
+                },
+                {
+                    'id': 16,
+                    'rentals': []
+                },
+                {
+                    'id': 17,
+                    'rentals': []
+                }
             ]
         }
 
