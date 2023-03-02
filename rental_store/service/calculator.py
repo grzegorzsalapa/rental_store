7a4836d5f9978d17564d51932160b772eb581192
Przemyslaw Boszczyk <przemyslaw.boszczyk@casumo.com>
2023-03-02 21:51:26 +0100

    Refactored calculator so that it's init'ed differently - by use of the constructor. Have not changed the tests in princpal - changed only those part responsible for creation of the calculator.

--- rental_store/service/calculator.py
+++ rental_store/service/calculator.py
@@ -2,54 +2,57 @@ from datetime import date
 
 from rental_store.models import Film, PriceList
 
-regular_flat_days = 3
-old_flat_days = 5
 
+class Calculator:
 
-def calculate_rent_charge(price_list: PriceList, film: Film, up_front_days: int):
-    if film.type == "New release":
+    def __init__(self, price_list: PriceList, regular_flat_days: int, old_flat_days: int):
+        self.price_list = price_list
+        self.regular_flat_days = regular_flat_days
+        self.old_flat_days = old_flat_days
 
-        charge = price_list.premium_price * up_front_days
+    def calculate_rent_charge(self, film: Film, up_front_days: int):
+        if film.type == "New release":
 
-        return charge, price_list.currency
+            charge = self.price_list.premium_price * up_front_days
 
-    elif film.type == "Regular":
+            return charge, self.price_list.currency
 
-        charge = max(regular_flat_days, up_front_days) * price_list.basic_price
+        elif film.type == "Regular":
 
-        return charge, price_list.currency
+            charge = max(self.regular_flat_days, up_front_days) * self.price_list.basic_price
 
-    elif film.type == "Old":
+            return charge, self.price_list.currency
 
-        charge = max(old_flat_days, up_front_days) * price_list.basic_price
+        elif film.type == "Old":
 
-        return charge, price_list.currency
+            charge = max(self.old_flat_days, up_front_days) * self.price_list.basic_price
 
+            return charge, self.price_list.currency
 
-def calculate_rent_surcharge(price_list: PriceList, film: Film, up_front_days: int, date_of_rent: date):
-    if film.type == "New release":
+    def calculate_rent_surcharge(self, film: Film, up_front_days: int, date_of_rent: date):
+        if film.type == "New release":
 
-        rent_duration = (date.today() - date_of_rent).days
-        overdue = max(0, rent_duration - up_front_days)
+            rent_duration = (date.today() - date_of_rent).days
+            overdue = max(0, rent_duration - up_front_days)
 
-        charge = overdue * price_list.premium_price
+            charge = overdue * self.price_list.premium_price
 
-        return charge, price_list.currency
+            return charge, self.price_list.currency
 
-    elif film.type == "Regular":
+        elif film.type == "Regular":
 
-        rent_duration = (date.today() - date_of_rent).days
-        overdue = max(0, rent_duration - max(regular_flat_days, up_front_days))
+            rent_duration = (date.today() - date_of_rent).days
+            overdue = max(0, rent_duration - max(self.regular_flat_days, up_front_days))
 
-        charge = overdue * price_list.basic_price
+            charge = overdue * self.price_list.basic_price
 
-        return charge, price_list.currency
+            return charge, self.price_list.currency
 
-    elif film.type == "Old":
+        elif film.type == "Old":
 
-        rent_duration = (date.today() - date_of_rent).days
-        overdue = max(0, rent_duration - max(old_flat_days, up_front_days))
+            rent_duration = (date.today() - date_of_rent).days
+            overdue = max(0, rent_duration - max(self.old_flat_days, up_front_days))
 
-        charge = overdue * price_list.basic_price
+            charge = overdue * self.price_list.basic_price
 
-        return charge, price_list.currency
+            return charge, self.price_list.currency
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,53 +1,53 @@
-import uuid
 from datetime import date, timedelta
 
 from rental_store.models import Film, PriceList
-from rental_store.service.calculator import calculate_rent_charge, calculate_rent_surcharge
+from rental_store.service.calculator import Calculator
+
+calculator = Calculator(PriceList(currency="SEK", premium_price=40, basic_price=30), 3, 5)
 
 
 def test_calculate_rent_charge_for_new_release():
+    # given
     def arrangement():
-        price_list = PriceList(currency="SEK", premium_price=40, basic_price=30)
         film = Film(id=0, title="Matrix 11", type="New release", items_total=50)
         up_front_days = 1
 
-        return price_list, film, up_front_days
+        return film, up_front_days
 
-    def action(price_list, film, up_front_days):
-        charge = calculate_rent_charge(price_list, film, up_front_days)
+    # when
+    def action(film, up_front_days):
+        charge = calculator.calculate_rent_charge(film, up_front_days)
 
         return charge
 
+    # then
     def assertion(charge):
         assert charge == (40, "SEK")
 
-    price_list, film, up_front_days = arrangement()
-    action_result = action(price_list, film, up_front_days)
+    film, up_front_days = arrangement()
+    action_result = action(film, up_front_days)
     assertion(action_result)
 
 
 def test_calculate_rent_surcharge_for_new_release():
     def arrangement():
-        price_list = PriceList(currency="SEK", premium_price=40, basic_price=30)
         film = Film(id=0, title="Matrix 11", type="New release", items_total=50)
-
-        request_id = uuid.uuid4()
         up_front_days = 1
 
         today = date.today()
         d = timedelta(days=3)
         date_of_rent = (today - d)
 
-        return price_list, film, up_front_days, date_of_rent
+        return film, up_front_days, date_of_rent
 
-    def action(price_list, film, up_front_days, date_of_rent):
-        charge = calculate_rent_surcharge(price_list, film, up_front_days, date_of_rent)
+    def action(film, up_front_days, date_of_rent):
+        charge = calculator.calculate_rent_surcharge(film, up_front_days, date_of_rent)
 
         return charge
 
     def assertion(charge):
         assert charge == (80, "SEK")
 
-    price_list, film, up_front_days, date_of_rent = arrangement()
-    action_result = action(price_list, film, up_front_days, date_of_rent)
+    film, up_front_days, date_of_rent = arrangement()
+    action_result = action(film, up_front_days, date_of_rent)
     assertion(action_result)
